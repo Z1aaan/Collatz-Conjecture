ad564a5e17482df549ed31661fbc4e66cc7d5d48
Z1aaan <zheanrobbyganituen@gmail.com>
2021-10-29 20:10:23 +0800

    PATH_latex

---
+++ collatz-PATH_latex.py
@@ -0,0 +1,15 @@
+from collatz import *
+
+
+def collatz_latex(collatz_N):
+    latex_path = str()
+    latex_start = '$'
+    latex_path += latex_path
+
+    latex_end = '$'
+    latex_end += latex_path
+
+
+if __name__ == "__main__":
+    print("""Put messages here later""")
+    collatz_latex(NUM)
--- collatz.py
+++ collatz.py
@@ -58,40 +58,6 @@ def collatzPath(NUM, PATH):  # creates the path for the user to view
             print(PATH)
             break
 
-def PATH_latex(NUM):
-    latexPath = str()
-    latex_start = '$'
-    collatz_function_latex = "f(n)=\left\{\begin{array}{ll}\frac{n}{2}, & \hspace{} n \, mod \, 2 = 0 \\3n+1, & \hspace{} n \, mod \, 2 = 1\\\end{array} \right"
-    latexPath += latex_start + collatz_function_latex
-
-    NUM_str = str(NUM)
-    collatz_function = "f(" + NUM_str + ")"
-    latex_start += "Path for " + collatz_function
-
-    NUM_str = str(NUM)
-    latexPath += "\\" + "n = " + NUM_str
-    equation = "\\" + NUM_str + " / 2 \\"
-    latexPath += equation
-    
-    while True:
-        if NUM != 1:
-            ifCondition = NUM % 2
-            if ifCondition == 0:
-                NUM = NUM/2
-                NUM_str = str(NUM)
-                latexPath += "\n" + "n = " + NUM_str
-                equation = "\n" + NUM_str + " / 2 \n"
-                latexPath += equation
-            else:
-                NUM = (3*NUM) + 1
-                NUM_str = str(NUM)
-                latexPath += "\n" + "n = " + NUM_str
-                equation = "(3 * " + NUM_str + ") + 1 \n"
-                latexPath += equation
-        else:
-            print(latexPath)
-            break
-
 
 if __name__ == "__main__":
     # create a variable to do the function on.
@@ -101,5 +67,3 @@ if __name__ == "__main__":
     collatz(NUM)
     t.sleep(sleepVar)
     collatzPath(NUM, PATH)
-    PATH_latex(NUM)
-    
