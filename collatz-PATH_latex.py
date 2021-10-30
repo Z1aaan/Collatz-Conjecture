9388965bca3c676b6260025b4e885faae39b4f2c
Z1aaan <zheanrobbyganituen@gmail.com>
2021-10-30 22:34:51 +0800

    added latex code for collatz function model

--- collatz-PATH_latex.py
+++ collatz-PATH_latex.py
@@ -3,12 +3,19 @@ from collatz import *
 
 def collatz_latex(collatz_N):
     latex_path = str()
+    collatz_function_form = """
+    f(n)=
+    \\begin{cases}
+    \\frac{n}{2}, & n \mod 2=0
+    \\\\
+    3n+1, &n \\mod 2=1
+    \\end{cases} \\\\
+    """
+    latex_path += collatz_function_form
 
-    latex_path += latex_path
-
-    latex_end += latex_path
+    print(latex_path)
 
 
 if __name__ == "__main__":
     print("""Put messages here later""")
-    collatz_latex(NUM)
+    collatz_latex(3)
