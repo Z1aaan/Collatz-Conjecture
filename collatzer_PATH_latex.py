74c2b850c3136645e7878f8229d6b81c4e653b5a
Z1aaan <zheanrobbyganituen@gmail.com>
2021-10-31 00:05:11 +0800

    added latexBegin and latexEnd

--- collatzer_PATH_latex.py
+++ collatzer_PATH_latex.py
@@ -1,21 +1,46 @@
 from collatzer_main import *
+import time as t
 
 
 def collatz_latex(collatz_N):
+    # remember to duplicate \ because python needs two \ to print it
     latex_path = str()
-    collatz_function_form = """
-    f(n)=
-    \\begin{cases}
-    \\frac{n}{2}, & n \mod 2=0
-    \\\\
-    3n+1, &n \\mod 2=1
-    \\end{cases} \\\\
-    """
-    latex_path += collatz_function_form
+    # start the latex docuement
+    latexBegin = """
+    \\documentclass{article}
+    \\usepackage[utf8]{inputenc}
+    \\usepackage{amsmath}
+    \\begin{document}
 
+    %Title
+    \\section{Collatz Sequence for \(n=[INPUT]\) }
+
+    %Collatz Function
+    \\[
+        f(n)=
+        \\begin{cases}
+        \\frac{n}{2}, & n \mod 2=0
+        \\\\
+        3n+1, &n \mod 2=1
+        \end{cases} \\\\
+    \\]"""
+    latex_path += latexBegin
+
+    # add the output of collatzer_main to the middle of the latex document
+
+    # end to latex docuement
+    latex_path += """
+    \\section{Credits and References Shit}
+    %Maybe add link to gitrepo and other shit, idk
+
+    \\end{document}"""
     print(latex_path)
 
 
 if __name__ == "__main__":
     print("""Put messages here later""")
+    for i in range(10):
+        # add this so that there's like a 'loading sequence' before the latex output is displayed
+        t.sleep(0.25)
+        print()
     collatz_latex(3)
